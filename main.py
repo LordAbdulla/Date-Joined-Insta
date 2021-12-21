669d60b47c74e73d295c9879e205b2bdb983e7a2
A B D U L L A H <74745688+LordAbdulla@users.noreply.github.com>
2021-12-21 17:23:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,15 +12,15 @@ print('''
             Tele : @DewTools | @LordAbdulla 
             ''')
 dewx = input(' Enter The UserName : ')
-head = {
+headers = {
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     'accept-encoding': 'gzip, deflate, br',
     'accept-language': 'en-US,en;q=0.9,ar;q=0.8',
     'cookie': f'csrftoken={secrets.token_hex(8)*2}; sessionid={secrets.token_hex(8)*2};',
     'user-agent': generate_user_agent(),
     }
-info_user = f'https://www.instagram.com/{dewx}/?__a=1'
-req = rs.get(info_user,headers=head).json()
+url = f'https://www.instagram.com/{dewx}/?__a=1'
+req = rs.get(url,headers=headers).json()
 dady= str(req['logging_page_id']).split('_')[1]
 
 lord = f'https://o7aa.pythonanywhere.com/?id={dady}'
@@ -32,5 +32,5 @@ print(f' [+] The Date That {dewx} Joined Insta Is {datee}')
 
 
 #Telegram : @DewTools
-#Inst : @hr8k
+#Inst : @hr8k 
 #Github : github.com/LordAbdulla
